3b92f9fc6f0fb2fc79186a867f7c7fb38c70b1e5
rahuldm778 <68591745+rahuldm778@users.noreply.github.com>
2022-09-25 10:17:49 +0530

    commite first

---
+++ src/DigitButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {ACTIONS} from './App'
+const DigitButton = (props) => {
+    return (
+        <button onClick={() => props.dispatch({type:ACTIONS.ADD_DIGIT, payload:{digit:props.digit}})}>{props.digit}</button>
+    );
+}
+
+export default DigitButton;
